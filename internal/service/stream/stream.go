66baecf6b8502808b9dec5a191cc228f98fa6259
syauqi sabili <syauqisabili@gmail.com>
2024-12-14 15:42:19 +0700

    func start_stream and stop_stream have been tested

--- internal/service/stream/stream.go
+++ internal/service/stream/stream.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"stream-session-api/domain"
 	"stream-session-api/internal/conf/network"
 	"stream-session-api/internal/repository"
 	pb "stream-session-api/internal/service/stream/proto"
 	"stream-session-api/pkg"
+	"strings"
 
 	"github.com/go-resty/resty/v2"
 
@@ -34,7 +36,7 @@ func (*Server) StartStream(ctx context.Context, in *pb.StartStreamRequest) (*pb.
 
 	// Get the peer information from the context
 	client, _ := peer.FromContext(ctx)
-	pkg.LogInfo(fmt.Sprintf("%s requested stream for %s from %s", in.Username, in.GetStreamId(), client.Addr))
+	pkg.LogInfo(fmt.Sprintf("%s requested to start stream for %s from %s", in.Username, in.GetStreamId(), client.Addr))
 
 	// Stream request for specific id
 	stream := &domain.Stream{
@@ -45,7 +47,7 @@ func (*Server) StartStream(ctx context.Context, in *pb.StartStreamRequest) (*pb.
 	//* you must make a mapping between stream.id and rtsp.subpath ex => id: 001 to subpath: /74630a72-2612-478b-9c49-308c720aa619
 	//? Why ? Because, you should not stream a video from a server directly if stream.id==rtsp.subPath
 	//! Temporary, i make it to be the same
-	subPath := stream.Id
+	subPath := "/" + stream.Id
 
 	// Set body for http post
 	body, _ := json.Marshal(
@@ -112,6 +114,58 @@ func (*Server) StartStream(ctx context.Context, in *pb.StartStreamRequest) (*pb.
 }
 
 func (*Server) StopStream(ctx context.Context, in *pb.StopStreamRequest) (*emptypb.Empty, error) {
+	// Check value pb.StartStreamRequest
+	if in == nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"Invalid message request")
+	}
+
+	// Get the peer information from the context
+	client, _ := peer.FromContext(ctx)
+	pkg.LogInfo(fmt.Sprintf("%s requested to stop stream on %s from %s", in.Username, in.GetStreamUrl(), client.Addr))
+
+	// Get uuid
+	parsedUrl, err := url.ParseRequestURI(in.GetStreamUrl())
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"Invalid stream url request")
+	}
+	uuid := strings.TrimSuffix(parsedUrl.Path, "/")
+	uuid = strings.TrimPrefix(uuid, "/")
+
+	// Stop stream
+	httpClient := resty.New()
+	resp, err := httpClient.R().
+		SetContext(ctx).
+		Delete(fmt.Sprintf("http://%s:%d/v3/config/paths/delete/%s",
+			network.Get().MediaMtx.Http.Ip,
+			network.Get().MediaMtx.Http.Port,
+			uuid))
+
+	if err != nil {
+		pkg.LogError(err)
+		return nil, status.Errorf(
+			codes.Unavailable, "Failed to stop stream")
+	}
+	switch resp.StatusCode() {
+	case 400:
+		pkg.LogError(err)
+		return nil, status.Errorf(
+			codes.FailedPrecondition, "Failed to remove stream session: %d", resp.StatusCode())
+	case 500:
+		pkg.LogError(err)
+		return nil, status.Errorf(
+			codes.Unimplemented, "Failed to remove stream session: %d", resp.StatusCode())
+
+	}
+	// Delete uuid on redis
+	repo := repository.NewStream()
+	if err := repo.Delete(uuid); err != nil {
+		return nil, status.Errorf(
+			codes.Unknown, "Fail to close stream")
+	}
 
 	//! Temporary
 	return nil, nil
